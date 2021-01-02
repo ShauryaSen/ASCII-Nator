e53c1f4cc11e2ec1b9128144e18fb6aa70781aa9
vxm52 <vaibhav.s.malla@gmail.com>
2021-01-02 14:55:39 -0500

    Add simple image loader

--- src/ASCII/Controller.java
+++ src/ASCII/Controller.java
@@ -13,8 +13,12 @@ import javafx.event.ActionEvent;
 
 import java.awt.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.awt.Desktop;
+import java.net.URI;
+
 
 public class Controller implements Initializable {
 
@@ -22,9 +26,19 @@ public class Controller implements Initializable {
     @FXML private Label title, urlLabel;
     @FXML private TextField urlField;
 
-    @FXML public void asciiNate(ActionEvent event) throws IOException{
+
+    @FXML public void asciiNate(ActionEvent event) throws IOException, URISyntaxException {
         System.out.println("Image ASCII-Nated");
 
+        //Save user input URL
+        String url = urlField.getText();
+
+        //Open URL in browser
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            Desktop.getDesktop().browse(new URI(""+url));
+        }
+
+        //Load new page
         Parent root = FXMLLoader.load(Main.class.getResource("/ASCII/Scene2.fxml"));
         Stage stage = new Stage();
         Scene scene = new Scene(root);
